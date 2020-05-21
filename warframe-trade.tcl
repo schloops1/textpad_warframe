5caba0af42c44235289b1e45b69476dcf2a65b84
schloops1 <57406858+schloops1@users.noreply.github.com>
2020-05-21 19:59:12 +0200

    Create warframe-trade.tcl

---
+++ warframe-trade.tcl
@@ -0,0 +1,1670 @@
+!TCL=8497, schloops - mit
+!TITLE=WARFRAME-TRADE
+!SORT=N
+
+!TEXT=---WARFRAMES---
+//WARFRAMES\^
+!
+!TEXT=Ash
+[Ash]
+!
+!TEXT=Atlas
+[Atlas]
+!
+!TEXT=Banshee
+[Banshee]
+!
+!TEXT=BARUUK
+[BARUUK]
+!
+!TEXT=Chroma
+[Chroma]
+!
+!TEXT=Ember
+[Ember]
+!
+!TEXT=Equinox
+[Equinox]
+!
+!TEXT=Excalibur
+[Excalibur]
+!
+!TEXT=Frost
+[Frost]
+!
+!TEXT=Gara
+[Gara]
+!
+!TEXT=GARUDA
+[GARUDA]
+!
+!TEXT=Gauss
+[Gauss]
+!
+!TEXT=GRENDEL
+[GRENDEL]
+!
+!TEXT=HARROW
+[HARROW]
+!
+!TEXT=HILDRYN
+[HILDRYN]
+!
+!TEXT=Hydroid
+[Hydroid]
+!
+!TEXT=Inaros
+[Inaros]
+!
+!TEXT=Ivara
+[Ivara]
+!
+!TEXT=Khora
+[Khora]
+!
+!TEXT=Limbo
+[Limbo]
+!
+!TEXT=Loki
+[Loki]
+!
+!TEXT=Mag
+[Mag]
+!
+!TEXT=Mesa
+[Mesa]
+!
+!TEXT=Mirage
+[Mirage]
+!
+!TEXT=Nekros
+[Nekros]
+!
+!TEXT=Nezha
+[Nezha]
+!
+!TEXT=Nidus
+[Nidus]
+!
+!TEXT=Nova
+[Nova]
+!
+!TEXT=Nyx
+[Nyx]
+!
+!TEXT=Oberon
+[Oberon]
+!
+!TEXT=Octavia
+[Octavia]
+!
+!TEXT=Revenant
+[Revenant]
+!
+!TEXT=Rhino
+[Rhino]
+!
+!TEXT=Saryn
+[Saryn]
+!
+!TEXT=Titania
+[Titania]
+!
+!TEXT=Trinity
+[Trinity]
+!
+!TEXT=Valkyr
+[Valkyr]
+!
+!TEXT=Vauban
+[Vauban]
+!
+!TEXT=Volt
+[Volt]
+!
+!TEXT=Wisp
+[Wisp]
+!
+!TEXT=Wukong
+[Wukong]
+!
+!TEXT=Zephyr
+[Zephyr]
+!
+!TEXT=Ash_Prime
+[Ash Prime]
+!
+!TEXT=ATLAS_Prime
+[ATLAS Prime]
+!
+!TEXT=Banshee_Prime
+[Banshee Prime]
+!
+!TEXT=Chroma_Prime
+[Chroma Prime]
+!
+!TEXT=Ember_Prime
+[Ember Prime]
+!
+!TEXT=Equinox_Prime
+[Equinox Prime]
+!
+!TEXT=Excalibur_Prime
+[Excalibur Prime]
+!
+!TEXT=Frost_Prime
+[Frost Prime]
+!
+!TEXT=Hydroid_Prime
+[Hydroid Prime]
+!
+!TEXT=Ivara_Prime
+[Ivara Prime]
+!
+!TEXT=Limbo_Prime
+[Limbo Prime]
+!
+!TEXT=Loki_Prime
+[Loki Prime]
+!
+!TEXT=Mag_Prime
+[Mag Prime]
+!
+!TEXT=Mesa_Prime
+[Mesa Prime]
+!
+!TEXT=Mirage_Prime
+[Mirage Prime]
+!
+!TEXT=Nekros_Prime
+[Nekros Prime]
+!
+!TEXT=Nova_Prime
+[Nova Prime]
+!
+!TEXT=Nyx_Prime
+[Nyx Prime]
+!
+!TEXT=Oberon_Prime
+[Oberon Prime]
+!
+!TEXT=Rhino_Prime
+[Rhino Prime]
+!
+!TEXT=Saryn_Prime
+[Saryn Prime]
+!
+!TEXT=TITANIA_Prime
+[TITANIA Prime]
+!
+!TEXT=Trinity_Prime
+[Trinity Prime]
+!
+!TEXT=Valkyr_Prime
+[Valkyr Prime]
+!
+!TEXT=Vauban_Prime
+[Vauban Prime]
+!
+!TEXT=Volt_Prime
+[Volt Prime]
+!
+!TEXT=Wukong_Prime
+[Wukong Prime]
+!
+!TEXT=Zephyr_Prime
+[Zephyr Prime]
+!
+
+!TEXT=---SENTINELS---
+//SENTINELS\^
+!
+!TEXT=Carrier
+[Carrier]
+!
+!TEXT=Carrier_Prime
+[Carrier Prime]
+!
+!TEXT=Dethcube
+[Dethcube]
+!
+!TEXT=Dethcube_Prime
+[Dethcube Prime]
+!
+!TEXT=Diriga
+[Diriga]
+!
+!TEXT=Djinn
+[Djinn]
+!
+!TEXT=Helios
+[Helios]
+!
+!TEXT=Helios_Prime
+[Helios Prime]
+!
+!TEXT=Oxylus
+[Oxylus]
+!
+!TEXT=Shade
+[Shade]
+!
+!TEXT=Shade_Prisma
+[Shade_Prisma]
+!
+!TEXT=Taxon
+[Taxon]
+!
+!TEXT=Wyrm
+[Wyrm]
+!
+!TEXT=Wyrm_Prime
+[Wyrm Prime]
+!
+
+!TEXT=---PRIMARIES---
+//PRIMARIES\^
+!
+!TEXT=Acceltra
+[Acceltra]
+!
+!TEXT=Amprex
+[Amprex]
+!
+!TEXT=Arca_Plasmor
+[Arca Plasmor]
+!
+!TEXT=Argonak
+[Argonak]
+!
+!TEXT=Artemis
+[Artemis]
+!
+!TEXT=Astilla
+[Astilla]
+!
+!TEXT=Attica
+[Attica]
+!
+!TEXT=Basmu
+[Basmu]
+!
+!TEXT=Battacor
+[Battacor]
+!
+!TEXT=Baza
+[Baza]
+!
+!TEXT=Baza_Prime
+[Baza Prime]
+!
+!TEXT=Boar
+[Boar]
+!
+!TEXT=Boar_Prime
+[Boar Prime]
+!
+!TEXT=Boltor
+[Boltor]
+!
+!TEXT=Boltor_Prime
+[Boltor Prime]
+!
+!TEXT=Braton
+[Braton]
+!
+!TEXT=Braton_Prime
+[Braton Prime]
+!
+!TEXT=Braton Vandal
+[Braton Vandal]
+!
+!TEXT=Burston
+[Burston]
+!
+!TEXT=Burston_Prime
+[Burston Prime]
+!
+!TEXT=Buzlok
+[Buzlok]
+!
+!TEXT=Carmine_Penta
+[Carmine Penta]
+!
+!TEXT=Cernos
+[Cernos]
+!
+!TEXT=Cernos_Prime
+[Cernos Prime]
+!
+!TEXT=Convectrix
+[Convectrix]
+!
+!TEXT=Corinth
+[Corinth]
+!
+!TEXT=Corinth_Prime
+[Corinth Prime]
+!
+!TEXT=Daikyu
+[Daikyu]
+!
+!TEXT=Dera
+[Dera]
+!
+!TEXT=Dera_Vandal
+[Dera Vandal]
+!
+!TEXT=Dex_Sybaris
+[Dex Sybaris]
+!
+!TEXT=Drakgoon
+[Drakgoon]
+!
+!TEXT=Dread
+[Dread]
+!
+!TEXT=Exergis
+[Exergis]
+!
+!TEXT=Ferrox
+[Ferrox]
+!
+!TEXT=Flux_Rifle
+[Flux Rifle]
+!
+!TEXT=Fulmin
+[Fulmin]
+!
+!TEXT=Glaxion
+[Glaxion]
+!
+!TEXT=Glaxion_Vandal
+[Glaxion Vandal]
+!
+!TEXT=Gorgon
+[Gorgon]
+!
+!TEXT=Gorgon_Wraith
+[Gorgon Wraith]
+!
+!TEXT=Grakata
+[Grakata]
+!
+!TEXT=Grinlok
+[Grakata]
+!
+!TEXT=Harpak
+[Harpak]
+!
+!TEXT=Hek
+[Hek]
+!
+!TEXT=Hema
+[Hema]
+!
+!TEXT=Hind
+[Hind]
+!
+!TEXT=Ignis
+[Ignis]
+!
+!TEXT=Ignis_Wraith
+[Ignis Wraith]
+!
+!TEXT=Javlok
+[Javlok]
+!
+!TEXT=Karak
+[Karak]
+!
+!TEXT=Karak_Wraith
+[Karak Wraith]
+!
+!TEXT=Kohm
+[Kohm]
+!
+!TEXT=Komorex
+[Komorex]
+!
+!TEXT=Kuva_Bramma
+[Kuva Bramma]
+!
+!TEXT=Kuva_Chakkhurr
+[Kuva Chakkhurr]
+!
+!TEXT=Kuva_Drakgoon
+[Kuva Drakgoon]
+!
+!TEXT=Kuva_Hind
+[Kuva Hind]
+!
+!TEXT=Kuva_Karak
+[Kuva Karak]
+!
+!TEXT=Kuva_Kohm
+[Kuva Kohm]
+!
+!TEXT=Kuva_Ogris
+[Kuva Ogris]
+!
+!TEXT=Kuva_Quartakk
+[Kuva Quartakk]
+!
+!TEXT=Kuva_Tonkor
+[Kuva Tonkor]
+!
+!TEXT=Lanka
+[Lanka]
+!
+!TEXT=Latron
+[Latron]
+!
+!TEXT=Latron_Prime
+[Latron Prime]
+!
+!TEXT=Latron_Wraith
+[Latron Wraith]
+!
+!TEXT=Lenz
+[Lenz]
+!
+!TEXT=MK1-Braton
+[MK1-Braton]
+!
+!TEXT=MK1-Paris
+[MK1-Braton]
+!
+!TEXT=MK1-Strun
+[MK1-Strun]
+!
+!TEXT=Miter
+[MK1-Strun]
+!
+!TEXT=Mutalist_Cernos
+[Mutalist Cernos]
+!
+!TEXT=Mutalist_Quanta
+[Mutalist Quanta]
+!
+!TEXT=Nagantaka
+[Nagantaka]
+!
+!TEXT=Ogris
+[Ogris]
+!
+!TEXT=Opticor
+[Opticor]
+!
+!TEXT=Opticor_Vandal
+[Opticor Vandal]
+!
+!TEXT=Panthera
+[Panthera]
+!
+!TEXT=Paracyst
+[Paracyst]
+!
+!TEXT=Paris
+[Paris]
+!
+!TEXT=Paris_Prime
+[Paris Prime]
+!
+!TEXT=Penta
+[Penta]
+!
+!TEXT=Phage
+[Phage]
+!
+!TEXT=Phantasma
+[Phantasma]
+!
+!TEXT=Prisma_Gorgon
+[Prisma Gorgon]
+!
+!TEXT=Prisma Grakata
+[Prisma Grakata]
+!
+!TEXT=Prisma Grinlok
+[Prisma Grinlok]
+!
+!TEXT=Prisma_Tetra
+[Prisma Tetra]
+!
+!TEXT=Quanta
+[Quanta]
+!
+!TEXT=Quanta_Vandal
+[Quanta Vandal]
+!
+!TEXT=Quartakk
+[Quartakk]
+!
+!TEXT=Quellor
+[Quellor]
+!
+!TEXT=Rakta_Cernos
+[Rakta Cernos]
+!
+!TEXT=Rubico
+[Rubico]
+!
+!TEXT=Rubico_Prime
+[Rubico Prime]
+!
+!TEXT=Sancti_Tigris
+[Sancti Tigris]
+!
+!TEXT=Scourge
+[Scourge]
+!
+!TEXT=Secura_Penta
+[Secura Penta]
+!
+!TEXT=Shedu
+[Shedu]
+!
+!TEXT=Simulor
+[Simulor]
+!
+!TEXT=Snipetron
+[Snipetron]
+!
+!TEXT=Snipetron_Vandal
+[Snipetron Vandal]
+!
+!TEXT=Sobek
+[Sobek]
+!
+!TEXT=Soma
+[Soma]
+!
+!TEXT=Soma_Prime
+[Soma Prime]
+!
+!TEXT=Stradavar
+[Stradavar]
+!
+!TEXT=Stradavar_Prime
+[Stradavar Prime]
+!
+!TEXT=Strun
+[Strun]
+!
+!TEXT=Strun_Wraith
+[Strun Wraith]
+!
+!TEXT=Supra
+[Supra]
+!
+!TEXT=Supra_Vandal
+[Supra Vandal]
+!
+!TEXT=Sybaris
+[Sybaris]
+!
+!TEXT=Sybaris_Prime
+[Sybaris Prime]
+!
+!TEXT=Synapse
+[Synapse]
+!
+!TEXT=Synoid_Simulor
+[Synoid Simulor]
+!
+!TEXT=Telos_Boltor
+[Telos Boltor]
+!
+!TEXT=Tenora
+[Tenora]
+!
+!TEXT=Tetra
+[Tetra]
+!
+!TEXT=Tiberon
+[Tiberon]
+!
+!TEXT=Tiberon_Prime
+[Tiberon Prime]
+!
+!TEXT=Tigris
+[Tigris]
+!
+!TEXT=Tigris_Prime
+[Tigris Prime]
+!
+!TEXT=Tonkor
+[Tonkor]
+!
+!TEXT=Torid
+[Torid]
+!
+!TEXT=Vaykor_Hek
+[Vaykor Hek]
+!
+!TEXT=Vectis
+[Vectis]
+!
+!TEXT=Vectis_Prime
+[Vectis Prime]
+!
+!TEXT=Veldt
+[Veldt]
+!
+!TEXT=Vulkar
+[Vulkar]
+!
+!TEXT=Vulkar_Wraith
+[Vulkar Wraith]
+!
+!TEXT=Zarr
+[Zarr]
+!
+!TEXT=Zenith
+[Zenith]
+!
+!TEXT=Zhuge
+[Zhuge]
+!
+!TEXT=Zhuge_Prime
+[Zhuge]
+!
+
+!TEXT=---SECONDARIES---
+//SECONDARIES\^
+!
+!TEXT=Acrid
+[Acrid]
+!
+!TEXT=Afuris
+[Afuris]
+!
+!TEXT=Akarius
+[Akarius]
+!
+!TEXT=Akbolto
+[Akbolto]
+!
+!TEXT=Akbolto_Prime
+[Akbolto Prime]
+!
+!TEXT=Akbronco
+[Akbronco]
+!
+!TEXT=Akbronco_Prime
+[Akbronco Prime]
+!
+!TEXT=Akjagara
+[Akjagara]
+!
+!TEXT=Akjagara_Prime
+[Akjagara Prime]
+!
+!TEXT=Aklato
+[Aklato]
+!
+!TEXT=Aklex
+[Aklex]
+!
+!TEXT=Aklex_Prime
+[Aklex Prime]
+!
+!TEXT=Akmagnus
+[Akmagnus]
+!
+!TEXT=Aksomati
+[Aksomati]
+!
+!TEXT=Aksomati_Prime
+[Aksomati Prime]
+!
+!TEXT=Akstiletto
+[Akstiletto]
+!
+!TEXT=Akstiletto_Prime
+[Akstiletto Prime]
+!
+!TEXT=Akvasto
+[Akvasto]
+!
+!TEXT=Akvasto_Prime
+[Akvasto Prime]
+!
+!TEXT=Akzani
+[Akzani]
+!
+!TEXT=Angstrum
+[Angstrum]
+!
+!TEXT=Arca_Scisco
+[Arca Scisco]
+!
+!TEXT=Atomos
+[Atomos]
+!
+!TEXT=Azima
+[Azima]
+!
+!TEXT=Balefire
+[Balefire]
+!
+!TEXT=Ballistica
+[Ballistica]
+!
+!TEXT=Ballistica_Prime
+[Ballistica Prime]
+!
+!TEXT=Bolto
+[Bolto]
+!
+!TEXT=Brakk
+[Brakk]
+!
+!TEXT=Bronco
+[Bronco]
+!
+!TEXT=Bronco_Prime
+[Bronco Prime]
+!
+!TEXT=Castanas
+[Castanas]
+!
+!TEXT=Cestra
+[Cestra]
+!
+!TEXT=Cyanex
+[Cyanex]
+!
+!TEXT=Cycron
+[Cycron]
+!
+!TEXT=Despair
+[Despair]
+!
+!TEXT=Detron
+[Detron]
+!
+!TEXT=Dex_Furis
+[Dex Furis]
+!
+!TEXT=Dex_Pixia
+[Dex Pixia]
+!
+!TEXT=Dual_Cestra
+[Dual Cestra]
+!
+!TEXT=Dual_Toxocyst
+[Dual Toxocyst]
+!
+!TEXT=Embolist
+[Embolist]
+!
+!TEXT=Euphona_Prime
+[Euphona Prime]
+!
+!TEXT=Furis
+[Furis]
+!
+!TEXT=Fusilai
+[Fusilai]
+!
+!TEXT=Gammacor
+[Gammacor]
+!
+!TEXT=Hikou
+[Hikou]
+!
+!TEXT=Hikou_Prime
+[Hikou Prime]
+!
+!TEXT=Hystrix
+[Hystrix]
+!
+!TEXT=Knell
+[Knell]
+!
+!TEXT=Kohmak
+[Kohmak]
+!
+!TEXT=Kraken
+[Kraken]
+!
+!TEXT=Kulstar
+[Kulstar]
+!
+!TEXT=Kunai
+[Kunai]
+!
+!TEXT=Kuva_Brakk
+[Kuva Brakk]
+!
+!TEXT=Kuva_Kraken
+[Kuva Kraken]
+!
+!TEXT=Kuva_Nukor
+[Kuva Nukor]
+!
+!TEXT=Kuva_Seer
+[Kuva Seer]
+!
+!TEXT=Kuva_Twin_Stubbas
+[Kuva Twin_Stubbas]
+!
+!TEXT=Lato
+[Lato]
+!
+!TEXT=Lato_Prime
+[Lato Prime]
+!
+!TEXT=Lato_Vandal
+[Lato Vandal]
+!
+!TEXT=Lex
+[Lex]
+!
+!TEXT=Lex_Prime
+[Lex Prime]
+!
+!TEXT=MK1-Furis
+[MK1-Furis]
+!
+!TEXT=MK1-Kunai
+[MK1-Kunai]
+!
+!TEXT=Magnus
+[Magnus]
+!
+!TEXT=Mara Detron
+[Mara Detron]
+!
+!TEXT=Marelok
+[Marelok]
+!
+!TEXT=Nukor
+[Nukor]
+!
+!TEXT=Ocucor
+[Ocucor]
+!
+!TEXT=Pandero
+[Pandero]
+!
+!TEXT=Plinx
+[Plinx]
+!
+!TEXT=Pox
+[Pox]
+!
+!TEXT=Prisma_Angstrum
+[Prisma Angstrum]
+!
+!TEXT=Prisma_Twin_Gremlins
+[Prisma Twin Gremlins]
+!
+!TEXT=Pyrana
+[Pyrana]
+!
+!TEXT=Pyrana_Prime
+[Pyrana Prime]
+!
+!TEXT=Quatz
+[Quatz]
+!
+!TEXT=Rakta_Ballistica
+[Rakta Ballistica]
+!
+!TEXT=Regulators
+[Regulators]
+!
+!TEXT=Regulators_Prime
+[Regulators Prime]
+!
+!TEXT=Sancti_Castanas
+[Sancti Castanas]
+!
+!TEXT=Secura_Dual_Cestra
+[Secura Dual Cestra]
+!
+!TEXT=Seer
+[Seer]
+!
+!TEXT=Sicarus
+[Sicarus]
+!
+!TEXT=Sicarus_Prime
+[Sicarus Prime]
+!
+!TEXT=Sonicor
+[Sonicor]
+!
+!TEXT=Spectra_Held
+[Spectra Held]
+!
+!TEXT=Spectra_Vandal
+[Spectra Vandal]
+!
+!TEXT=Spira
+[Spira]
+!
+!TEXT=Spira_Prime
+[Spira Prime]
+!
+!TEXT=Staticor
+[Staticor]
+!
+!TEXT=Stubba
+[Stubba]
+!
+!TEXT=Stug
+[Stug]
+!
+!TEXT=Synoid_Gammacor
+[Synoid Gammacor]
+!
+!TEXT=Talons
+[Talons]
+!
+!TEXT=Telos_Akbolto
+[Telos Akbolto]
+!
+!TEXT=Twin Grakatas
+[Twin Grakatas]
+!
+!TEXT=Twin_Gremlins
+[Twin Gremlins]
+!
+!TEXT=Twin_Kohmak
+[Twin Kohmak]
+!
+!TEXT=Twin_Rogga
+[Twin Rogga]
+!
+!TEXT=Twin_Vipers
+[Twin Vipers]
+!
+!TEXT=Twin_Vipers_Wraith
+[Twin Vipers Wraith]
+!
+!TEXT=Tysis
+[Tysis]
+!
+!TEXT=Vasto
+[Vasto]
+!
+!TEXT=Vasto_Prime
+[Vasto Prime]
+!
+!TEXT=Vaykor_Marelok
+[Vaykor Marelok]
+!
+!TEXT=Viper
+[Viper]
+!
+!TEXT=Viper_Wraith
+[Viper Wraith]
+!
+!TEXT=Zakti
+[Zakti]
+!
+!TEXT=Zylok
+[Zylok]
+!
+
+!TEXT=---MELEE WEAPONS---
+//MELEE WEAPONS\^
+!
+!TEXT=Ack_&_Brunt
+[Ack & Brunt]
+!
+!TEXT=Amphis
+[Amphis]
+!
+!TEXT=Anku
+[Anku]
+!
+!TEXT=Ankyros
+[Ankyros]
+!
+!TEXT=Ankyros_Prime
+[Ankyros Prime]
+!
+!TEXT=Arca_Titron
+[Arca Titron]
+!
+!TEXT=Atterax
+[Atterax]
+!
+!TEXT=Bo
+[Bo]
+!
+!TEXT=Bo_Prime
+[Bo Prime]
+!
+!TEXT=Boltace
+[Boltace]
+!
+!TEXT=Broken
+[Broken]
+!
+!TEXT=Cadus
+[Cadus]
+!
+!TEXT=Cassowar
+[Cassowar]
+!
+!TEXT=Caustacyst
+[Caustacyst]
+!
+!TEXT=Ceramic
+[Ceramic]
+!
+!TEXT=Cerata
+[Cerata]
+!
+!TEXT=Ceti_Lacera
+[Ceti Lacera]
+!
+!TEXT=Cobra_&_Crane
+[Cobra & Crane]
+!
+!TEXT=Cronus
+[Cronus]
+!
+!TEXT=Dakra_Prime
+[Dakra Prime]
+!
+!TEXT=Dark_Dagger
+[Dark Dagger]
+!
+!TEXT=Dark_Split-Sword
+[Dark Split-Sword]
+!
+!TEXT=Dark_Sword
+[Dark Sword]
+!
+!TEXT=Desert_Wind
+[Desert Wind]
+!
+!TEXT=Destreza
+[Destreza]
+!
+!TEXT=Destreza_Prime
+[Destreza Prime]
+!
+!TEXT=Dex_Dakra
+[Dex Dakra]
+!
+!TEXT=Diwata
+[Diwata]
+!
+!TEXT=Dragon_Nikana
+[Dragon Nikana]
+!
+!TEXT=Dual_Cleavers
+[Dual Cleavers]
+!
+!TEXT=Dual_Ether
+[Dual Ether]
+!
+!TEXT=Dual_Heat_Swords
+[Dual Heat Swords]
+!
+!TEXT=Dual_Ichor
+[Dual Ichor]
+!
+!TEXT=Dual_Kamas
+[Dual Kamas]
+!
+!TEXT=Dual_Kamas_Prime
+[Dual Kamas Prime]
+!
+!TEXT=Dual_Keres
+[Dual Keres]
+!
+!TEXT=Dual_Raza
+[Dual Raza]
+!
+!TEXT=Dual_Skana
+[Dual Skana]
+!
+!TEXT=Dual_Zoren
+[Dual Zoren]
+!
+!TEXT=Endura
+[Endura]
+!
+!TEXT=Ether_Daggers
+[Ether Daggers]
+!
+!TEXT=Ether_Reaper
+[Ether Reaper]
+!
+!TEXT=Ether_Sword
+[Ether Sword]
+!
+!TEXT=Exalted_Blade
+[Exalted Blade]
+!
+!TEXT=Falcor
+[Falcor]
+!
+!TEXT=Fang
+[Fang]
+!
+!TEXT=Fang_Prime
+[Fang Prime]
+!
+!TEXT=Fragor
+[Fragor]
+!
+!TEXT=Fragor_Prime
+[Fragor Prime]
+!
+!TEXT=Furax
+[Furax]
+!
+!TEXT=Furax Wraith
+[Furax Wraith]
+!
+!TEXT=Galatine
+[Galatine]
+!
+!TEXT=Galatine_Prime
+[Galatine Prime]
+!
+!TEXT=Galvacord
+[Galvacord]
+!
+!TEXT=Garuda's_Talons
+[Garuda's Talons]
+!
+!TEXT=Gazal_Machete
+[Gazal Machete]
+!
+!TEXT=Glaive
+[Glaive]
+!
+!TEXT=Glaive_Prime
+[Glaive Prime]
+!
+!TEXT=Gram
+[Gram]
+!
+!TEXT=Gram_Prime
+[Gram Prime]
+!
+!TEXT=Guandao
+[Guandao]
+!
+!TEXT=Gunsen
+[Gunsen]
+!
+!TEXT=Halikar
+[Halikar]
+!
+!TEXT=Hate
+[Hate]
+!
+!TEXT=Heat_Dagger
+[Heat Dagger]
+!
+!TEXT=Heat_Sword
+[Heat Sword]
+!
+!TEXT=Heliocor
+[Heliocor]
+!
+!TEXT=Hirudo
+[Hirudo]
+!
+!TEXT=Iron
+[Iron]
+!
+!TEXT=Jat_Kittag
+[Jat Kittag]
+!
+!TEXT=Jat_Kusar
+[Jat Kusar]
+!
+!TEXT=Jaw_Sword
+[Jaw Sword]
+!
+!TEXT=Kama
+[Kama]
+!
+!TEXT=Karyst
+[Karyst]
+!
+!TEXT=Kesheg
+[Kesheg]
+!
+!TEXT=Kestrel
+[Kestrel]
+!
+!TEXT=Kogake
+[Kogake]
+!
+!TEXT=Kogake_Prime
+[Kogake Prime]
+!
+!TEXT=Korrudo
+[Korrudo]
+!
+!TEXT=Kreska
+[Kreska]
+!
+!TEXT=Krohkur
+[Krohkur]
+!
+!TEXT=Kronen
+[Kronen]
+!
+!TEXT=Kronen_Prime
+[Kronen Prime]
+!
+!TEXT=Kuva_Shildeg
+[Kuva Shildeg]
+!
+!TEXT=Lacera
+[Lacera]
+!
+!TEXT=Lecta
+[Lecta]
+!
+!TEXT=Lesion
+[Lesion]
+!
+!TEXT=MK1-Bo
+[MK1-Bo]
+!
+!TEXT=MK1-Furax
+[MK1-Furax]
+!
+!TEXT=Machete
+[Machete]
+!
+!TEXT=Machete_Wraith
+[Machete Wraith]
+!
+!TEXT=Magistar
+[Magistar]
+!
+!TEXT=Masseter
+[Masseter]
+!
+!TEXT=Mios
+[Mios]
+!
+!TEXT=Mire
+[Mire]
+!
+!TEXT=Nami_Skyla
+[Nami Skyla]
+!
+!TEXT=Nami_Skyla_Prime
+[Nami Skyla Prime]
+!
+!TEXT=Nami_Solo
+[Nami Solo]
+!
+!TEXT=Nikana
+[Nikana]
+!
+!TEXT=Nikana_Prime
+[Nikana Prime]
+!
+!TEXT=Ninkondi
+[Ninkondi]
+!
+!TEXT=Ninkondi_Prime
+[Ninkondi Prime]
+!
+!TEXT=Obex
+[Obex]
+!
+!TEXT=Ohma
+[Ohma]
+!
+!TEXT=Okina
+[Okina]
+!
+!TEXT=Orthos
+[Orthos]
+!
+!TEXT=Orthos_Prime
+[Orthos Prime]
+!
+!TEXT=Orvius
+[Orvius]
+!
+!TEXT=Pangolin
+[Pangolin]
+!
+!TEXT=Pangolin_Prime
+[Pangolin Prime]
+!
+!TEXT=Paracesis
+[Paracesis]
+!
+!TEXT=Pathocyst
+[Pathocyst]
+!
+!TEXT=Pennant
+[Pennant]
+!
+!TEXT=Plasma_Sword
+[Plasma Sword]
+!
+!TEXT=Prisma_Dual Cleavers
+[Prisma Dual Cleavers]
+!
+!TEXT=Prisma_Machete
+[Prisma Machete]
+!
+!TEXT=Prisma_Obex
+[Prisma Obex]
+!
+!TEXT=Prisma_Skana
+[Prisma Skana]
+!
+!TEXT=Prova
+[Prova]
+!
+!TEXT=Prova_Vandal
+[Prova Vandal]
+!
+!TEXT=Pupacyst
+[Pupacyst]
+!
+!TEXT=Rakta_Dark_Dagger
+[Rakta Dark Dagger]
+!
+!TEXT=Reaper_Prime
+[Reaper Prime]
+!
+!TEXT=Redeemer
+[Redeemer]
+!
+!TEXT=Redeemer_Prime
+[Redeemer Prime]
+!
+!TEXT=Ripkas
+[Ripkas]
+!
+!TEXT=Sancti_Magistar
+[Sancti Magistar]
+!
+!TEXT=Sarpa
+[Sarpa]
+!
+!TEXT=Scindo
+[Scindo]
+!
+!TEXT=Scindo_Prime
+[Scindo Prime]
+!
+!TEXT=Scoliac
+[Scoliac]
+!
+!TEXT=Secura_Lecta
+[Secura Lecta]
+!
+!TEXT=Serro
+[Serro]
+!
+!TEXT=Shaku
+[Shaku]
+!
+!TEXT=Sheev
+[Shaku]
+!
+!TEXT=Sibear
+[Sibear]
+!
+!TEXT=Sigma_&_Octantis
+[Sigma & Octantis]
+!
+!TEXT=Silva_&_Aegis
+[Silva & Aegis]
+!
+!TEXT=Silva_&_Aegis_Prime
+[Silva & Aegis Prime]
+!
+!TEXT=Skana
+[Skana]
+!
+!TEXT=Skana_Prime
+[Skana Prime]
+!
+!TEXT=Skiajati
+[Skiajati]
+!
+!TEXT=Sydon
+[Sydon]
+!
+!TEXT=Synoid_Heliocor
+[Synoid Heliocor]
+!
+!TEXT=Tatsu
+[Tatsu]
+!
+!TEXT=Tekko
+[Tekko]
+!
+!TEXT=Tekko_Prime
+[Tekko Prime]
+!
+!TEXT=Telos_Boltace
+[Telos Boltace]
+!
+!TEXT=Tipedo
+[Tipedo]
+!
+!TEXT=Tipedo_Prime
+[Tipedo Prime]
+!
+!TEXT=Tonbo
+[Tonbo]
+!
+!TEXT=Twin_Basolk
+[Twin Basolk]
+!
+!TEXT=Twin_Krohkur
+[Twin Krohkur]
+!
+!TEXT=Exalted_Blade
+[Exalted Blade]
+!
+!TEXT=Unarmed
+[Unarmed]
+!
+!TEXT=Valkyr_Talons
+[Valkyr Talons]
+!
+!TEXT=Vaykor_Sydon
+[Vaykor Sydon]
+!
+!TEXT=Venka
+[Venka]
+!
+!TEXT=Venka_Prime
+[Venka Prime]
+!
+!TEXT=Volnus
+[Volnus]
+!
+!TEXT=War
+[War]
+!
+!TEXT=Wolf
+[War]
+!
+!TEXT=Zenistar
+[Zenistar]
+!
+
+!TEXT=---SENTINEL WEAPONS---
+//SENTINEL WEAPONS\^
+!
+!TEXT=Artax
+[Artax]
+!
+!TEXT=Burst_Laser
+[Burst Laser]
+!
+!TEXT=Cryotra_Held
+[Cryotra Held]
+!
+!TEXT=Deconstructor
+[Deconstructor]
+!
+!TEXT=Deconstructor_Prime
+[Deconstructor Prime]
+!
+!TEXT=Deth_Machine_Rifle
+[Deth Machine Rifle]
+!
+!TEXT=Deth_Machine_Rifle_Prime
+[Deth Machine Rifle Prime]
+!
+!TEXT=Laser_Rifle
+[Laser Rifle]
+!
+!TEXT=Multron
+[Multron]
+!
+!TEXT=Prime_Laser_Rifle
+[Prime Laser Rifle]
+!
+!TEXT=Prisma_Burst_Laser
+[Prisma Burst Laser]
+!
+!TEXT=Stinger
+[Stinger]
+!
+!TEXT=Sweeper
+[Sweeper]
+!
+!TEXT=Sweeper_Prime
+[Sweeper Prime]
+!
+!TEXT=Tazicor
+[Tazicor]
+!
+!TEXT=Vulcax
+[Vulcax]
+!
+!TEXT=Vulklok
+[Vulklok]
+!
+
+!TEXT=---ARCHFRAME RANGED---
+//ARCHFRAME RANGED\^
+!
+!TEXT=Corvas
+[Corvas]
+!
+!TEXT=Cyngas
+[Cyngas]
+!
+!TEXT=Dual_Decurion
+[Dual Decurion]
+!
+!TEXT=Fluctus
+[Fluctus]
+!
+!TEXT=Grattler
+[Grattler]
+!
+!TEXT=Imperator
+[Imperator]
+!
+!TEXT=Imperator_Vandal
+[Imperator Vandal]
+!
+!TEXT=Kuva_Ayanga
+[Kuva Ayanga]
+!
+!TEXT=Larkspur
+[Larkspur]
+!
+!TEXT=Phaedra
+[Phaedra]
+!
+!TEXT=Velocitus
+[Velocitus]
+!
+
+!TEXT=---ARCHFRAME MELEE---
+//ARCHFRAME MELEE\^
+!
+!TEXT=Agkuza
+[Agkuza]
+!
+!TEXT=Centaur
+[Centaur]
+!
+!TEXT=Kaszas
+[Kaszas]
+!
+!TEXT=Knux
+[Knux]
+!
+!TEXT=Onorix
+[Onorix]
+!
+!TEXT=Prisma_Veritux
+[Prisma Veritux]
+!
+!TEXT=Rathbone
+[Rathbone]
+!
+!TEXT=Veritux
+[Veritux]
+!
+
+!TEXT=---PRIME PARTS---
+//PRIME PARTS\^
+!
+!TEXT=blueprint
+blueprint
+!
+!TEXT=chassis
+chassis
+!
+!TEXT=systems
+systems
+!
+!TEXT=cerebrum
+cerebrum
+!
+
+!TEXT=---RELIC TYPE---
+//RELIC TYPE\^
+!
+!TEXT=lith
+lith
+!
+!TEXT=meso
+lith
+!
+!TEXT=neo
+neo
+!
+!TEXT=axi
+axi
+!
+
+!TEXT=---RELIC RARITY---
+//RELIC RARITY\^
+!
+!TEXT=Common
+Common
+!
+!TEXT=Uncommon
+Uncommon
+!
+!TEXT=Rare
+Rare
+!
